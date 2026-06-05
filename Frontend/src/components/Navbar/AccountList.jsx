db0bff72fb74376294e7df2b6ea08b0d5e191979
octaveweb <karansproject955@gmail.com>
2026-06-05 16:47:52 +0530

    Add AccountList component and update Navbar styles; refactor CSS for account container

---
+++ Frontend/src/components/Navbar/AccountList.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const AccountList = () => {
+    return (
+        <ul className='list-item'>
+            <li>
+                <span>Account</span>
+                <i class="ri-external-link-line"></i>
+            </li>
+            <li>
+                <span>Portfolio</span>
+            </li>
+            <li>
+                <span>Recents</span>
+            </li>
+            <li>
+                <span>Support</span>
+                <i class="ri-external-link-line"></i>
+            </li>
+            <li>
+                <span>Privet session</span>
+            </li>
+            <li>
+                <span>Settings</span>
+            </li>
+            <li>
+                <span>Log out</span>
+            </li>
+
+        </ul>
+    )
+}
+
+export default AccountList
--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import Tooltip from '../Custome/Tooltip'
+import AccountList from './AccountList'
+
+
+
+
 const Navbar = () => {
+
+  const [Acount, setAcount] = useState(false)
+  const [text, setText] = useState("Account")
   return (
     <nav>
       <div className="right">
         <span> <Tooltip content={<i className="ri-arrow-left-s-line"></i>} overlya="Go Back" /></span>
         <span>  <Tooltip content={<i className="ri-arrow-right-s-line"></i>} overlya="Go forword" /></span>
-        
-        
+
+
       </div>
       <div className="middle">
         <Tooltip content={<i className="ri-home-2-fill" id="home"></i>} overlya="Home" />
         <div className="search">
           <Tooltip content={<i className="ri-search-line"></i>} overlya="Search" />
-          
+
           <input type="taxt" name="search" id="search" placeholder='What do you want to play?' />
 
           <div className="shortcut-key">
@@ -23,17 +31,32 @@ const Navbar = () => {
           </div>
 
           <span className="divider"></span>
-          
+
           <Tooltip content={<i className="ri-archive-stack-line"></i>} overlya="Browse" />
         </div>
       </div>
       <div className="left">
-        
+
         <Tooltip content={<i className="ri-notification-2-line"></i>} overlya="Whatt's New" />
-        
+
         <Tooltip content={<i className="ri-team-line"></i>} overlya="Listening activity" />
-        
-        <Tooltip content={<button className="login">K</button>} overlya="Account" />
+
+        <ul className='account-contaner'>
+          <li
+          onClick={()=>{
+            if (Acount == false){
+              setAcount(true)
+              setText(" ")
+            }else{
+              setAcount(false)
+              setText("Account")
+            }
+          }}
+          >
+            <Tooltip content={<button className="login">K</button>} overlya={text}  />
+           {Acount==true? <AccountList /> : " " } 
+          </li>
+        </ul>
       </div>
     </nav>
   )
